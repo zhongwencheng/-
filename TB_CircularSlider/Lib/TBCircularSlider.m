b00a635f349d0727b3b06af3ed8f5f9928ac063e
钟文成(外包) <ex-zhongwencheng001@zhongwenheng.local>
2016-07-26 15:49:31 +0800

    第一次提交

---
+++ TB_CircularSlider/Lib/TBCircularSlider.m
@@ -0,0 +1,295 @@
+//
+//  TBCircularSlider.m
+//  TB_CircularSlider
+//
+//  Created by Yari Dareglia on 1/12/13.
+//  Copyright (c) 2013 Yari Dareglia. All rights reserved.
+//
+
+#import "TBCircularSlider.h"
+#import "Commons.h"
+
+/** Helper Functions **/
+/*弧度转角度*/
+#define ToRad(deg) 		( (M_PI * (deg)) / 180.0 )
+/*角度转弧度*/
+#define ToDeg(rad)		( (180.0 * (rad)) / M_PI )
+#define SQR(x)			( (x) * (x) )
+
+/** Parameters **/
+#define TB_SAFEAREA_PADDING 60
+
+
+#pragma mark - Private -
+
+@interface TBCircularSlider(){
+    UITextField *_textField;
+    int radius;
+}
+@end
+
+
+#pragma mark - Implementation -
+
+@implementation TBCircularSlider
+
+-(id)initWithFrame:(CGRect)frame{
+    
+    self = [super initWithFrame:frame];
+    
+    if(self){
+        self.opaque = NO;
+        
+        //Define the circle radius taking into account the safe area
+        radius = self.frame.size.width/2 - TB_SAFEAREA_PADDING;
+        
+        //Initialize the Angle at 0
+        self.angle = 360;
+        
+        
+        //Define the Font
+        UIFont *font = [UIFont fontWithName:TB_FONTFAMILY size:TB_FONTSIZE];
+        //Calculate font size needed to display 3 numbers
+        NSString *str = @"000";
+        CGSize fontSize = [str sizeWithFont:font];
+        
+        //Using a TextField area we can easily modify the control to get user input from this field
+        _textField = [[UITextField alloc]initWithFrame:CGRectMake((frame.size.width  - fontSize.width) /2,
+                                                                  (frame.size.height - fontSize.height) /2,
+                                                                  fontSize.width,
+                                                                  fontSize.height)];
+        _textField.backgroundColor = [UIColor clearColor];
+        _textField.textColor = [UIColor colorWithWhite:1 alpha:0.8];
+        _textField.textAlignment = NSTextAlignmentCenter;
+        _textField.font = font;
+        _textField.text = [NSString stringWithFormat:@"%d",self.angle];
+        _textField.enabled = NO;
+        
+        [self addSubview:_textField];
+    }
+    
+    return self;
+}
+
+
+#pragma mark - UIControl Override -
+
+/** Tracking is started **/
+-(BOOL)beginTrackingWithTouch:(UITouch *)touch withEvent:(UIEvent *)event{
+    [super beginTrackingWithTouch:touch withEvent:event];
+    
+    //We need to track continuously
+    return YES;
+}
+
+/** Track continuos touch event (like drag) **/
+-(BOOL)continueTrackingWithTouch:(UITouch *)touch withEvent:(UIEvent *)event{
+    [super continueTrackingWithTouch:touch withEvent:event];
+
+    //Get touch location
+    CGPoint lastPoint = [touch locationInView:self];
+
+    //Use the location to design the Handle
+    [self movehandle:lastPoint];
+    
+    //Control value has changed, let's notify that   
+    [self sendActionsForControlEvents:UIControlEventValueChanged];
+   
+    return YES;
+}
+
+/** Track is finished **/
+-(void)endTrackingWithTouch:(UITouch *)touch withEvent:(UIEvent *)event{
+    [super endTrackingWithTouch:touch withEvent:event];
+    
+}
+
+
+#pragma mark - Drawing Functions - 
+
+//Use the draw rect to draw the Background, the Circle and the Handle 
+-(void)drawRect:(CGRect)rect{
+    
+    [super drawRect:rect];
+    
+    CGContextRef ctx = UIGraphicsGetCurrentContext();
+    
+/** Draw the Background **/
+    
+    //Create the path
+    CGContextAddArc(ctx, self.frame.size.width/2, self.frame.size.height/2, radius, 0, M_PI *2, 0);
+    
+    //Set the stroke color to black
+    [[UIColor blackColor]setStroke];
+    
+    //Define line width and cap
+    CGContextSetLineWidth(ctx, TB_BACKGROUND_WIDTH);
+    CGContextSetLineCap(ctx, kCGLineCapButt);
+    
+    //draw it!
+    CGContextDrawPath(ctx, kCGPathStroke);
+    
+   
+//** Draw the circle (using a clipped gradient) **/
+    
+    
+    /** Create THE MASK Image **/
+    UIGraphicsBeginImageContext(CGSizeMake(TB_SLIDER_SIZE,TB_SLIDER_SIZE));
+    CGContextRef imageCtx = UIGraphicsGetCurrentContext();
+    
+    CGContextAddArc(imageCtx, self.frame.size.width/2  , self.frame.size.height/2, radius, 0, ToRad(self.angle), 0);
+    [[UIColor redColor]set];
+    
+    //Use shadow to create the Blur effect
+    CGContextSetShadowWithColor(imageCtx, CGSizeMake(0, 0), self.angle/20, [UIColor blackColor].CGColor);
+    
+    //define the path
+    CGContextSetLineWidth(imageCtx, TB_LINE_WIDTH);
+    CGContextDrawPath(imageCtx, kCGPathStroke);
+    
+    //save the context content into the image mask
+    CGImageRef mask = CGBitmapContextCreateImage(UIGraphicsGetCurrentContext());
+    UIGraphicsEndImageContext();
+    
+    
+
+    /** Clip Context to the mask **/
+    CGContextSaveGState(ctx);
+    
+    CGContextClipToMask(ctx, self.bounds, mask);
+    CGImageRelease(mask);
+    
+    
+    
+    /** THE GRADIENT **/
+    
+    //list of components渐变的颜色的范围
+    CGFloat components[] = {
+        0.0, 0.0, 1.0, 1.0,     // Start color - Blue
+        1.0, 0.0, 1.0, 1.0,
+        0.0, 1.0, 0.0, 1.0
+    };   // End color - Violet
+    
+    CGColorSpaceRef baseSpace = CGColorSpaceCreateDeviceRGB();
+    /*
+     baseSpace：颜色空间，也就是要上色的空间类型（这里是rgb类型的上色空间）
+     components：这个是要上什么样的色
+     NULL：这个参数这里上传的nil，但是实际上是一个locate，表示哪种从哪个个色值开始渐变
+     3:这个参数表示目前用3种色值，红，蓝，绿，也可以传2，表示只用了红和蓝两个色
+     */
+    CGGradientRef gradient = CGGradientCreateWithColorComponents(baseSpace, components, NULL, 3);
+    CGColorSpaceRelease(baseSpace), baseSpace = NULL;
+    
+    //Gradient direction颜色渐变的方向
+    CGPoint startPoint = CGPointMake(CGRectGetMidX(rect), CGRectGetMinY(rect));
+    CGPoint endPoint = CGPointMake(CGRectGetMidX(rect), CGRectGetMaxY(rect));
+    
+    //Draw the gradient
+    CGContextDrawLinearGradient(ctx, gradient, startPoint, endPoint, 0);
+    CGGradientRelease(gradient), gradient = NULL;
+    
+    CGContextRestoreGState(ctx);
+    
+    
+/** Add some light reflection effects on the background circle**/
+    
+    CGContextSetLineWidth(ctx, 1);
+    CGContextSetLineCap(ctx, kCGLineCapRound);
+    
+    //Draw the outside light
+    CGContextBeginPath(ctx);
+    CGContextAddArc(ctx, self.frame.size.width/2  , self.frame.size.height/2, radius+TB_BACKGROUND_WIDTH/2, 0, ToRad(-self.angle), 1);
+    [[UIColor colorWithWhite:1.0 alpha:0.05]set];
+    CGContextDrawPath(ctx, kCGPathStroke);
+    
+    //draw the inner light
+    CGContextBeginPath(ctx);
+    CGContextAddArc(ctx, self.frame.size.width/2  , self.frame.size.height/2, radius-TB_BACKGROUND_WIDTH/2, 0, ToRad(-self.angle), 1);
+    [[UIColor colorWithWhite:1.0 alpha:0.05]set];
+    CGContextDrawPath(ctx, kCGPathStroke);
+    
+    
+/** Draw the handle **/
+    [self drawTheHandle:ctx];
+    
+}
+
+/** Draw a white knob over the circle **/
+-(void) drawTheHandle:(CGContextRef)ctx{
+    
+    CGContextSaveGState(ctx);
+    
+    //I Love shadows
+    CGContextSetShadowWithColor(ctx, CGSizeMake(0, 0), 3, [UIColor blackColor].CGColor);
+    
+    //Get the handle position
+    CGPoint handleCenter =  [self pointFromAngle: self.angle];
+    
+    //Draw It!
+    [[UIColor colorWithWhite:1.0 alpha:0.7]set];
+    CGContextFillEllipseInRect(ctx, CGRectMake(handleCenter.x, handleCenter.y, TB_LINE_WIDTH, TB_LINE_WIDTH));
+    
+    CGContextRestoreGState(ctx);
+}
+
+
+#pragma mark - Math -
+
+/** Move the Handle **/
+-(void)movehandle:(CGPoint)lastPoint{
+    
+    //Get the center
+    CGPoint centerPoint = CGPointMake(self.frame.size.width/2, self.frame.size.height/2);
+    
+    //Calculate the direction from a center point and a arbitrary position.
+    float currentAngle = AngleFromNorth(centerPoint, lastPoint, NO);
+    int angleInt = floor(currentAngle);
+    
+    //Store the new angle
+    self.angle = 360 - angleInt;
+    //Update the textfield 
+    _textField.text =  [NSString stringWithFormat:@"%d", self.angle];
+    
+    //Redraw
+    [self setNeedsDisplay];
+}
+
+/** Given the angle, get the point position on circumference **/
+//根据角度计算点的坐标
+-(CGPoint)pointFromAngle:(int)angleInt{
+    
+    //Circle center
+    CGPoint centerPoint = CGPointMake(self.frame.size.width/2 - TB_LINE_WIDTH/2, self.frame.size.height/2 - TB_LINE_WIDTH/2);
+    
+    //The point position on the circumference
+    /*
+     ceil(x)返回不小于x的最小整数值（然后转换为double型）。
+     floor(x)返回不大于x的最大整数值。
+     round(x)返回x的四舍五入整数值。
+     */
+    CGPoint result;
+    /*这个地方用到的三角函数的概念，有点绕*/
+    result.y = round(centerPoint.y + radius * sin(ToRad(-angleInt))) ;
+    result.x = round(centerPoint.x + radius * cos(ToRad(-angleInt)));
+    
+    return result;
+}
+
+//Sourcecode from Apple example clockControl 
+//Calculate the direction in degrees from a center point to an arbitrary position.
+// 钟文成  这个函数是算出两个点之间的夹角
+static inline float AngleFromNorth(CGPoint p1, CGPoint p2, BOOL flipped) {
+    CGPoint v = CGPointMake(p2.x-p1.x,p2.y-p1.y);
+    //三角函数
+    float vmag = sqrt(SQR(v.x) + SQR(v.y)), result = 0;
+    v.x /= vmag;//sina
+    v.y /= vmag;//sinb
+    //算出正切值
+    double radians = atan2(v.y,v.x);
+    //将正切值转化成角度
+    result = ToDeg(radians);
+    return (result >=0  ? result : result + 360.0);
+}
+@end
+
+
